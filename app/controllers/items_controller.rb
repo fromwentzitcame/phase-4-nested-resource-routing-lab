7acc7ff71be4474e2b95143e89a1dbae35cd63f4
Alex Wentz <wentz.alex@gmail.com>
2021-11-28 18:01:09 -0500

    commit tree 4db7d34df00f7b5a5c14f1a64c94927100e3d3eb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,39 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_response
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = find_user
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item, include: :user
+  end
+
+  def create
+    user = find_user
+    item = user.items.create(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def find_user
+    User.find(params[:user_id])
+  end
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def record_not_found_response(issue)
+      render json: { error: "#{issue.model} not found" }, status: :not_found
   end
 
 end
